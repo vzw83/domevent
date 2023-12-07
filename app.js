fc8cacb831030bbf44133076c34da950626cad4d
vzw83 <vzw83@yandex.ru>
2023-12-07 10:50:10 +0300

    finishTask

--- app.js
+++ app.js
@@ -2,20 +2,28 @@ const EXP = document.querySelector("#experiment");
 const CR = document.querySelector("#current-result");
 const RL = document.querySelector("#result-list");
 
-EXP.addEventListener("click", (event) => {
+
+
+
+const createAction = (event) => {
+  const newRow = document.createElement('p')
+    newRow.innerText = event.type  
+
+
   CR.innerText = event.type;
+  RL.prepend(newRow);
+};
+//перечень событий мышки
+EXP.addEventListener("click", createAction); //клик
+EXP.addEventListener("dblclick", createAction); //дабл клик
+EXP.addEventListener("contextmenu", createAction); //клик правой кнопкой мыши
 
-  RL.innerHTML += '<p>' + event.type + '</p>'
-});
+EXP.addEventListener("mousover", createAction); //наведение мышью на элемент
+EXP.addEventListener("mouseenter", createAction); // событие когда только навели мышью
+EXP.addEventListener("mouseleave", createAction);//событие убираем наведение мышью
 
-EXP.addEventListener("dblclick", (event) => {
-  CR.innerText = event.type;
 
-  RL.innerHTML += '<p>' + event.type + '</p>'
-});
+EXP.addEventListener("focus", createAction);//событие убираем наведение мышью
+EXP.addEventListener("blur", createAction);//событие убираем наведение мышью
 
-EXP.removeEventListener("click", (event) => {
-  CR.innerText = event.type;
 
-  RL.innerHTML += '<p>' + event.type + '</p>'
-});
